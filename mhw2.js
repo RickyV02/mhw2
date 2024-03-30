05ac2ea730de66b141b45f4c17f624a8c6f280c9
RickyV02 <VLLRCR02E30C351Q@studium.unict.it>
2024-03-30 10:29:52 +0100

    aggiunte stelline boxad

--- mhw2.js
+++ mhw2.js
@@ -1,5 +1,6 @@
 const loginitem = document.querySelector(".login");
 const cross_img = document.querySelectorAll(".cross");
+const boxadreviews = document.querySelectorAll(".boxad span");
 
 function login() {
   const window = document.getElementById("loginwindow");
@@ -15,4 +16,27 @@ function close(event) {
 
 for (const crosses of cross_img) {
   crosses.addEventListener("click", close);
+}
+
+function hidereviews(event) {
+  const element = event.currentTarget;
+  element.addEventListener("click", showreviews);
+  const elementdiv = event.currentTarget.parentNode;
+  const revs = elementdiv.querySelector("p");
+  revs.remove();
+}
+
+function showreviews(event) {
+  const element = event.currentTarget;
+  const elementdiv = event.currentTarget.parentNode;
+  const new_text = document.createElement("p");
+  new_text.classList.add("inforev");
+  new_text.textContent = element.dataset.info;
+  elementdiv.appendChild(new_text);
+  element.removeEventListener("click", showreviews);
+  element.addEventListener("click", hidereviews);
+}
+
+for (const rev of boxadreviews) {
+  rev.addEventListener("click", showreviews);
 }
