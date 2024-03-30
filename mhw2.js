77e87f6daaf013be3e720baf38cc9e6e1381012f
RickyV02 <VLLRCR02E30C351Q@studium.unict.it>
2024-03-30 11:26:49 +0100

    aggiunto cambio ov poplist

--- mhw2.js
+++ mhw2.js
@@ -1,6 +1,7 @@
 const loginitem = document.querySelector(".login");
 const cross_img = document.querySelectorAll(".cross");
 const boxadreviews = document.querySelectorAll(".boxad span");
+const overviews = document.querySelectorAll(".ac img");
 
 function login() {
   const window = document.getElementById("loginwindow");
@@ -39,4 +40,36 @@ function showreviews(event) {
 
 for (const rev of boxadreviews) {
   rev.addEventListener("click", showreviews);
+}
+
+function resetoverviews(event) {
+  const image = event.currentTarget;
+  const index = parseInt(image.dataset.index);
+  if (index === 1) {
+    image.src = "public/films1.png";
+  } else if (index === 2) {
+    image.src = "public/films2.png";
+  } else {
+    image.src = "public/films3.png";
+  }
+  image.removeEventListener("mouseover", resetoverviews);
+  image.addEventListener("mouseover", changeoverviews);
+}
+
+function changeoverviews(event) {
+  const image = event.currentTarget;
+  const index = parseInt(image.dataset.index);
+  if (index === 1) {
+    image.src = "public/ov1.png";
+  } else if (index === 2) {
+    image.src = "public/ov2.png";
+  } else {
+    image.src = "public/ov3.png";
+  }
+  image.removeEventListener("mouseover", changeoverviews);
+  image.addEventListener("mouseover", resetoverviews);
+}
+
+for (const ov of overviews) {
+  ov.addEventListener("mouseover", changeoverviews);
 }
