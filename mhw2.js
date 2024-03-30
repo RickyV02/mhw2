41d8e0675da9ceb564ee5f955bca161c4e6b4fb3
RickyV02 <VLLRCR02E30C351Q@studium.unict.it>
2024-03-30 09:06:12 +0100

    aggiunto box login

--- mhw2.js
+++ mhw2.js
@@ -1,4 +1,4 @@
-const loginitems = document.querySelectorAll(".login");
+const loginitem = document.querySelector(".login");
 const cross_img = document.querySelectorAll(".cross");
 
 function login() {
@@ -6,13 +6,11 @@ function login() {
   window.classList.remove("nascosto");
 }
 
+loginitem.addEventListener("click", login);
+
 function close(event) {
   const element = event.currentTarget.parentNode;
   element.classList.add("nascosto");
-}
-
-for (const logitem of loginitems) {
-  logitem.addEventListener("click", login);
 }
 
 for (const crosses of cross_img) {
